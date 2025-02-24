d4b744455fb9907e2e89d7f40368a56f4e0dfca4
Tat-T <ring_87_87@mail.ru>
2025-02-24 17:40:37 +0300

    Работает отправка данных с формы брони в БД

---
+++ Pages/Zakaz.cshtml.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyRazorApp.Data;
+using System.Threading.Tasks;
+
+namespace MyRazorApp.Pages
+{
+    public class ZakazModel : PageModel
+    {
+        private readonly AppDbContext _context;
+
+        public ZakazModel(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Reservation Reservation { get; set; } = new();
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Reservations.Add(Reservation);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Success");
+        }
+    }
+}
